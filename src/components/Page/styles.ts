6cca5309e3c6cdbd07e9e8457d7fc0d0ca421aee
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-31 16:36:16 -0300

    feat: applied image in the background of the site

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -1,26 +1,74 @@
 import styled from "styled-components";
+const Model3 = require("../../images/Model3.jpg");
+const ModelY = require("../../images/ModelY.jpg");
+const ModelS = require("../../images/ModelS.jpg");
+const ModelX = require("../../images/ModelX.jpg");
+const SolarPanels = require("../../images/SolarPanels.jpg");
+const SolarRoof = require("../../images/SolarRoof.jpg");
+const Accessories = require("../../images/Accessories.jpg");
 
 export const Container = styled.div`
   .colored:nth-child(1) {
-    background: #f1ffe7;
+    background: url("${Model3}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @media (max-width: 600px) {
+      background-position: 40%;
+    }
   }
   .colored:nth-child(2) {
-    background: #dfffd9;
+    background: url("${ModelY}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
   .colored:nth-child(3) {
-    background: #cdfeca;
+    background: url("${ModelS}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
   }
   .colored:nth-child(4) {
-    background: #bbfebb;
+    background: url("${ModelX}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @media (max-width: 600px) {
+      background-position: 60%;
+    }
   }
   .colored:nth-child(5) {
-    background: #a9fdac;
+    background: url("${SolarPanels}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @media (max-width: 600px) {
+      background-position: 40%;
+    }
   }
   .colored:nth-child(6) {
-    background: #90f29c;
+    background: url("${SolarRoof}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @media (max-width: 600px) {
+      background-position: 40%;
+    }
   }
   .colored:nth-child(7) {
-    background: #77e68c;
+    background: url("${Accessories}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    @media (max-width: 600px) {
+      background-position: left;
+    }
   }
 `;
 
